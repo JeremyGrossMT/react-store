cb9b2c9bd091ebb848914f89632f3de01a30b1f0
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2019-07-06 17:10:27 -0600

    Added a Sign in page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+           email: '',
+           password: '' 
+        }
+    }
+
+		handelChange = event => {
+			const { value, name } = event.target;
+			
+			this.setState({[name]: value})
+		}
+
+		handelSubmit = event => {
+			event.preventDefault();
+			this.setState({email: '', password: ''})
+		}
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handelSubmit}>
+                  <FormInput 
+										name='email' 
+										type='email' 
+										value={this.state.email} 
+										handelChange={this.handelChange} 
+										label='email'
+										required 
+									/>
+                  
+                  <FormInput 
+										name='password' 
+										type='password' 
+										value={this.state.password} 
+										handelChange={this.handelChange} 
+										label='password'
+										required
+									/>
+                  
+									<CustomButton type='Submit' value='Submit From'> Sign In </CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
