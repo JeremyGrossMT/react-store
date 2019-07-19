22c8a1999dd523b857444785006e51282a1d5587
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2019-07-19 06:48:32 -0600

    Added redux-persist to site for local storage

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -11,14 +11,12 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       <div className='image-container'>
         <img src={imageUrl} alt='item' />
       </div>
-      <span className='name'>{name}</span>
-     
+      <span className='name'>{name}</span>     
       <span className='quantity'>
         <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
         <span className='value'>{quantity}</span>
         <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
-      </span>
-      
+      </span>      
       <span className='price'>{price}</span>
       <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
     </div>
@@ -30,6 +28,5 @@ const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item)),
   removeItem: item => dispatch(removeItem(item))
 })
-
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>
 , document.getElementById('root'));
