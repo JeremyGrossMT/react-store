cd98b3f4f3f89ea0bfbb1f104a8bebaf4595d337
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2019-07-20 11:22:32 -0600

    TypeError: Cannot read property 'title' of undefined on the collection.component.jsx:10

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,7 +2,7 @@ import SHOP_DATA from './shop.data';
 
 const INITIAL_STATE = {
   collections: SHOP_DATA
-}
+};
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,24 +1,14 @@
 import { createSelector } from 'reselect';
 
-const COLECTION_ID_MAP = {
-  hats: 1,
-  sneackers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-[selectShop],
+  [selectShop],
   shop => shop.collections
 );
 
-export const selectCollection = collectionURLParam => 
-createSelector(
-  [selectCollections],
-    collections => collections.find(
-      collection => collection.id === COLECTION_ID_MAP[collectionURLParam]
-    )
-  );
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+);
