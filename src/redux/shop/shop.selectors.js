c0d8b12c8096c3d60789cd6f5f885fab005583c4
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2019-07-20 09:45:18 -0600

    Added Collection Page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,24 @@
 import { createSelector } from 'reselect';
 
+const COLECTION_ID_MAP = {
+  hats: 1,
+  sneackers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-  [selectShop],
+[selectShop],
   shop => shop.collections
 );
+
+export const selectCollection = collectionURLParam => 
+createSelector(
+  [selectCollections],
+    collections => collections.find(
+      collection => collection.id === COLECTION_ID_MAP[collectionURLParam]
+    )
+  );
