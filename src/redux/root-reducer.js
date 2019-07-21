d54d3f70a6a90a0f5725ca26c63b1d203e9b1281
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2019-07-20 18:16:47 -0600

    Fixed the Title error in the root-reducer.js file

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -10,7 +10,7 @@ import shopReducer from './shop/shop.reducer'
 const persistConfig = {
   key: 'root',
   storage,
-  whilelist: ['cart']
+  whitelist: ['cart']
 }
 
 const rootReducer = combineReducers({
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import { persistStore } from 'redux-persist'
-import rootReducer from './root-reducer'
+import { persistStore } from 'redux-persist';
+import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
