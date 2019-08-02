40212c6acd19365254704f1c540e27f9dabf11db
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2019-08-01 20:59:18 -0600

    Added Rudux-thunk to site

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { connect } from 'react-redux'
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import './collections-overview.styles.scss';
 
-const CollectionsOverview  = ({ collections })  => (
-    <div className='collections-overview'>
-        {collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-    </div>
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
+
+const CollectionsOverview = ({ collections }) => (
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview
+  collections: selectCollectionsForPreview
 });
-  
-  export default connect(mapStateToProps)(CollectionsOverview );
+
+export default connect(mapStateToProps)(CollectionsOverview);
